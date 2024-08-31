fe8664a1dd3e47e77b09924ec9a2a370d49d0e86
mbarto202 <michael.d.barto@gmail.com>
2024-08-31 17:45:50 -0400

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello');
+    }
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => handleClickAgain('michael', e)}>Click Me Again</button>
         </div>
      );
 }
