fbd85a4f1af23937afb7fe424b8c18e0556863e9
mbarto202 <michael.d.barto@gmail.com>
2024-08-31 16:31:42 -0400

    dynamic values in templates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  // const person = {name: 'michael', age: 25};
+  const link = "http://www.google.com";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Compenents</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+
+        {/*<p>{person}</p>*/}
+
+        <p>{10}</p>
+        <p>{'hello'}</p>
+        <p>{[1, 2, 3]}</p>
+        <p>{Math.random()*10}</p>
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
