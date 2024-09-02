a99f009deae5c545989e6927e58b34d4e7e52a99
mbarto202 <michael.d.barto@gmail.com>
2024-09-02 13:49:38 -0400

    conditional loading

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-
-    const [name, setName] = useState('Michael')
+    const [isPending, setIsPending] = useState(true);
 
    
 
@@ -14,11 +13,13 @@ const Home = () => {
         })
         .then(data => {
             setBlogs(data)
+            setIsPending(false);
         })
     }, [])
 
     return ( 
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs ={blogs} title="All Blogs"/>}
         </div>
      );
