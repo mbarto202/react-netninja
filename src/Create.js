0ec9e5fb5ec051a8ed7f6496d8765fa0bfa0abd3
mbarto202 <michael.d.barto@gmail.com>
2024-09-24 22:52:49 -0400

    pending message

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,13 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("yoshi");
+  const [isPending, setIsPending] = useState('false');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
+
+    setIsPending(true);
 
     fetch(`http://localhost:8000/blogs`,{
       method: 'POST',
@@ -15,6 +18,7 @@ const Create = () => {
       body: JSON.stringify(blog)
     }).then(() => {
       console.log('new blog added');
+      setIsPending(false);
     })
   };
 
